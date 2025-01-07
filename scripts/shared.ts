bd9948b1725b18e2f7ac5cbe1ff69d45fce5253b
XantreDev <valerik007.vs@gmail.com>
2025-01-07 22:46:33 +0100

    implemented counter

--- scripts/shared.ts
+++ scripts/shared.ts
@@ -1,11 +1,26 @@
+import {
+  Address,
+  beginCell,
+  Cell,
+  contractAddress,
+  StateInit,
+} from "@ton/core";
+import compiledContract from "../build/main.compiled.json" with { type: "json" };
 
-import { Cell, contractAddress, StateInit } from '@ton/core'
-import compiledContract from '../build/main.compiled.json' with {type: 'json'}
-
-export const TESTNET = !process.env.MAINNET
-const getCodeCell = ()=> Cell.fromHex(compiledContract.hex)
-export const getStateInit = (): StateInit => ({
+export const TESTNET = !process.env.MAINNET;
+const getCodeCell = () => Cell.fromHex(compiledContract.hex);
+export type StateConfig = {
+  initialCount: number;
+  initialAddress: Address;
+};
+export const getStateInit = (config: StateConfig) =>
+  ({
     code: getCodeCell(),
-    data: Cell.EMPTY
-  })
-export const getAddress = () => contractAddress(0, getStateInit())
+    data: beginCell()
+      .storeUint(config.initialCount, 32)
+      .storeAddress(config.initialAddress)
+      .endCell(),
+  }) satisfies StateInit;
+
+export const getAddress = (config: StateConfig) =>
+  contractAddress(0, getStateInit(config));
--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,70 +1,88 @@
-import "@ton/test-utils"
+import "@ton/test-utils";
 
-import {describe, expect, it} from 'vitest'
-import compiledContract from '../build/main.compiled.json' with {type: 'json'}
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode, toNano } from '@ton/core'
-import {Blockchain, BlockchainContractProvider, internal} from '@ton/sandbox'
-
+import { describe, expect, it } from "vitest";
+import compiledContract from "../build/main.compiled.json" with { type: "json" };
+import {
+  Address,
+  beginCell,
+  Cell,
+  Contract,
+  contractAddress,
+  ContractProvider,
+  Sender,
+  SendMode,
+  toNano,
+} from "@ton/core";
+import { Blockchain, BlockchainContractProvider, internal } from "@ton/sandbox";
+import { getStateInit, StateConfig } from "../scripts/shared";
 
 class MainContract implements Contract {
   constructor(
     readonly address: Address,
-    readonly init?: {code: Cell; data: Cell}
+    readonly init?: { code: Cell; data: Cell }
   ) {}
-  
-  static create(code: Cell): MainContract {
-    const workchain = 0;
-    const data = beginCell().endCell()
-    const init = {code, data}
 
-    return new MainContract(contractAddress(workchain, init), init)
+  static create(config: StateConfig): MainContract {
+    const workchain = 0;
+    const init = getStateInit(config);
+
+    return new MainContract(contractAddress(workchain, init), init);
   }
-  
+
   // we must use classes because, most of sandbox methods uses prototype chain modification to work
-  async sendInternalMessage(contractProvider: ContractProvider, sender: Sender, value: bigint) {
-    await contractProvider.internal(sender, ({
+  async sendInternalMessage(
+    contractProvider: ContractProvider,
+    sender: Sender,
+    value: bigint,
+    body: Cell = Cell.EMPTY
+  ) {
+    await contractProvider.internal(sender, {
       sendMode: SendMode.PAY_GAS_SEPARATELY,
       value,
-      // sendMode: SendMode.PAY_GAS_SEPARATELY,
-      body: beginCell().endCell()
-    }))
+      body,
+    });
   }
   async getData(provider: ContractProvider) {
-    const {stack} = await provider.get("get_the_latest_sender", [])
-    return {
-      recentSender: stack.readAddress()
-    }
+    const { stack } = await provider.get("get_contract_storage_data", []);
+
+    const amount = stack.readNumber();
+    const address = stack.readAddress();
+
+    return { amount, recentSender: address };
   }
 }
 
-// const MainContract_sendInternalMessage = async (
-//     blockchain: Blockchain,
-//     sender: Address,
-//     receiver: Address,
-//     value: bigint,
-//   ) => 
-
-// const MainContract_getData = async (provider: ContractProvider) => {
-// }
-
 describe("main.fc contract tests", () => {
-  const hexContractCode = compiledContract.hex 
-
-  const codeCell = Cell.fromHex(hexContractCode)
   it("aboba", async () => {
-    const blockchain = await Blockchain.create()
-    const contract = await blockchain.openContract(MainContract.create(codeCell))
-    const senderWallet = await blockchain.treasury('sender')
-    
-    const transaction = await contract.sendInternalMessage(senderWallet.getSender(), toNano("0.05"))
+    const blockchain = await Blockchain.create();
+    const initialAddress = blockchain.treasury("initialAddress");
+    const initialCount = 0;
+    const increment_by = 220;
+    const contract = await blockchain.openContract(
+      MainContract.create({
+        initialCount: initialCount,
+        initialAddress: (await initialAddress).address,
+      })
+    );
+    const senderWallet = await blockchain.treasury("sender");
+
+    const transaction = await contract.sendInternalMessage(
+      senderWallet.getSender(),
+      toNano("0.05"),
+      beginCell()
+        .storeUint(/* opcode */ 1, 32)
+        .storeUint(/* increment_by */ increment_by, 32)
+        .endCell()
+    );
 
     expect(transaction.transactions).transaction({
       from: senderWallet.address,
       to: contract.address,
-      success: true
-    })
-    
-    const {recentSender} = await contract.getData()
-    expect(recentSender.toString()).toBe(senderWallet.address.toString())
-  })
-})
+      success: true,
+    });
+
+    const { recentSender, amount } = await contract.getData();
+    expect(recentSender.toString()).toBe(senderWallet.address.toString());
+    expect(amount).toBe(initialCount + increment_by)
+  });
+});
